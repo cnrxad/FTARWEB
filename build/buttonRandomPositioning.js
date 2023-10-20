2a9ebcdfe77eef179e131f3777f4ff5d08d546cc
cnrxad <pabloisyo@outlook.com>
2023-10-20 23:15:34 +0200

    cnrxad

--- build/buttonRandomPositioning.js
+++ build/buttonRandomPositioning.js
@@ -2,6 +2,6 @@ const imageButton = document.querySelectorAll('.imageButton');
 
 imageButton.forEach(btn =>{
   console.log(btn);
-  btn.style.top = Math.floor(Math.random()*1920) + 'px';
-    btn.style.left = Math.floor(Math.random()*2000) + 'px';
+  btn.style.top = Math.floor(Math.random()*100) + '%';
+    btn.style.left = Math.floor(Math.random()*100) + '%';
 })
